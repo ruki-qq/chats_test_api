c8fa94860693603c029ad795bacd5e3b11887033
fern <mitaka@duck.com>
2026-01-17 18:51:31 +0300

    fix import error

--- src/core/models/message.py
+++ src/core/models/message.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from types import TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import DateTime, CheckConstraint, ForeignKey, String, func
